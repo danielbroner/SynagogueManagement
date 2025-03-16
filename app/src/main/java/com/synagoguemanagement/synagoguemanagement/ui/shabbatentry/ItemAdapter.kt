c17e651006f1259d4adcc6a19fdcfa673bd3919f
Daniel Broner <dbroner@fortinet.com>
2025-03-16 21:30:03 +0200

    TASK-3 - Create shabbat entries page

---
+++ app/src/main/java/com/synagoguemanagement/synagoguemanagement/ui/shabbatentry/ItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.synagoguemanagement.synagoguemanagement.ui.shabbatentry
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.synagoguemanagement.synagoguemanagement.R
+
+class ItemAdapter(private val items: List<String>) : RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.shabbat_entry_card, parent, false)
+        return ItemViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val title: TextView = itemView.findViewById(R.id.shabbat_entry_title)
+        private val start: TextView = itemView.findViewById(R.id.shabbat_entry_start)
+        private val exit: TextView = itemView.findViewById(R.id.shabbat_entry_exit)
+        fun bind(item: String) {
+            title.text = item
+            start.text = "Entry: Friday, 5:34 PM"
+            exit.text = "Exit: Saturday, 6:48 PM"
+        }
+    }
+}
---
+++ app/src/main/java/com/synagoguemanagement/synagoguemanagement/ui/shabbatentry/ShabbatEntryFragment.kt
@@ -0,0 +1,38 @@
+package com.synagoguemanagement.synagoguemanagement.ui.shabbatentry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.synagoguemanagement.synagoguemanagement.R
+import com.synagoguemanagement.synagoguemanagement.databinding.FragmentShabbatEntryBinding
+
+class ShabbatEntryFragment  : Fragment() {
+    private var _binding: FragmentShabbatEntryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentShabbatEntryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Sample list of items
+        val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5", "Item 5")
+
+        // Set RecyclerView properties
+        binding.shabbatEntries.layoutManager = LinearLayoutManager(requireContext())
+        binding.shabbatEntries.adapter = ItemAdapter(items)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
